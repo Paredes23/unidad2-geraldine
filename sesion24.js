6e7add2510fd5b5b8ec7a2cc5468e42f2f7ad75f
Paredes23 <jparedes@estudiantes.crackthecode.la>
2021-11-12 11:15:21 -0500

    estos son lod documentos que avanzamos en clase(practica)

---
+++ sesion24.js
@@ -0,0 +1,20 @@
+/* 
+for (var i=o; i<10; i++){
+    alert(i)
+}
+*/
+/*
+let text = "";
+for (let i=0; i<10; i++){
+    text +=i+ "<br>";
+}
+Document.getElementById("demo").innerHTML =text;
+*/
+
+const cars = ["BMW", "VOLVO", "Saab","Ford","Fiat","Audi"];
+let text="";
+for (let i=0; i<cars.length; length i++){
+    text +=cars[i] + "<br>";
+}
+document.getElementById("demo").innerHTML = text;
+
