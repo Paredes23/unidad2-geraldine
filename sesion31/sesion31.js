955bcd7267a5068b234f3e64941504cedd571982
Paredes23 <jparedes@estudiantes.crackthecode.la>
2021-11-23 14:29:00 -0500

    Este es el avance en cada clase(practica

--- sesion31/sesion31.js
+++ sesion31/sesion31.js
@@ -1,81 +1,86 @@
-/// validacion de tarjeta de credito//
-//dato mayoria tiene 16 digitos//
-function validarTarjeta(){
-  let alerta = document.getElementById("alerta");
-  alerta.innerText="";
-  alerta.className="";
+// ----------------------- VALIDACIÓN DE TARJETA DE CRÉDITO -----------------------
+// Dato: La mayoría de las tarjetas tienen 16 dígitos.
+function validarTarjeta() {
+    // Nos debemos asegurar de que la alerta no sea visible antes de terminar de validar.
+    let alerta = document.getElementById("alerta");
+    alerta.innerText = "";
+    alerta.className = "";
 
-  let numero = document.getElementById("numero-tarjeta");
-  let digitosTC = numero.value;
-  if (digitosTC.length == 0){
-      alerta.innerText = "Tarjeta Invalida";
-      alerta.className = "alerta-error"; 
-          return false;
-   }
-
-  //PASO 1: ALMACENAR EN UN ARREGLO LOS DIGITOS EN ORDEN INVERSO
-   let arrTC = Array.from(digitosTC);//Almacena cada caracter de los numeros de la tarjeta de credito///
-   let arrInverso = arrTC.reverse();//Invertir el orden de los elementos del arreglos//
-   //PASO 2:MULTIPLICAR LOS ELEMENTOS EN POSICIONES PARES8contando desde el ultimo)
-   //si el numero resultante es de 2 digitos, se debe sumar los digitos
-
-
-   let digitoPar = -1;
-    for(let i = 1;i<= arrInverso.length; i+=2){
-    digitoPar = parseInt(arrInverso[i]);
-    digitoPar *= 2;
-
-    if (digitoPar >= 10){
-        digitoPar = digitoPar.toString();
-        digitoPar = parseInt(digitoPar[1]) + parseInt(digitoPar[0]);
+    let numero = document.getElementById("numero-tarjeta");
+    let digitosTC = numero.value;
+    if (digitosTC.length == 0) {
+        alerta.innerText = "Tarjeta Inválida";
+        alerta.className = "alerta-error";
+        return false;
     }
-    arrInverso[i]= digitoPar.toString();
-    
-  }
-//Paso 3: Sumar todos los digitos de las tarjeta
 
-  let suma = 0;
-  for (let j =0;j<arrInverso.length; j++)
-      if (typeof arrInverso[j] == "string"){
-        suma += parseInt(arrInverso[j]);     
+    // PASO 1: ALMACENAR EN UN ARREGLO LOS DIGITOS EN ORDEN INVERSO
+    let arrTC = Array.from(digitosTC); // Almacena cada caracter de los numeros de la tarjeta
+    let arrInverso = arrTC.reverse(); // Invertir el orden de los elementos del arreglo
+
+    // PASO 2: Multiplicar los elementos en posiciones pares ( contando desde el ultimo)
+    // Si el numero resultante es de 2 digitos, se debe sumar los digitos
+
+    let digitoPar = -1;
+    for (let i = 1; i <= arrInverso.length; i += 2) {
+        digitoPar = parseInt(arrInverso[i]);
+        digitoPar *= 2;
+
+        if (digitoPar >= 10) {
+            digitoPar = digitoPar.toString();
+            digitoPar = parseInt(digitoPar[1]) + parseInt(digitoPar[0]);
+        }
+        arrInverso[i] = digitoPar.toString();
+    }
+
+    //PASO 3: Sumar todos los digitos de la tarjeta
+
+    let suma =0;
+    for (let j =0; j<arrInverso.length; j++){
+        if(typeof arrInverso[j] == "string"){
+            suma += parseInt(arrInverso[j]);
+
+        }else {
+            suma += arrInverso[j];
+        }
+    }
+//Paso 4: Verificar si el resultado es divisible por 10
+
+   if (suma % 10 == 0) {
+        alerta.innerText = "Tarjeta Válida";
+        alerta.className = "alerta-valida";
+        return true;
     } else {
-      suma += arrInverso[j];
+        alerta.innerText = "Tarjeta Inválida";
+        alerta.className = "alerta-error";
+        return false
+        ;
     }
-}
 
-//PASO 4: VERIFICAR SI EL RESULTADO ES DIVISIBLE POR 10
-
-    if (suma %10 == 0){
-    alerta.innerText = "Tarjeta valida";
-    alerta.className = "alerta-valida";
-    return true; 
-}   else {
-  alerta.innerText = "Tarjeta Invalida";
-  alerta.className = "alerta-error";
-  return false;
-}
-
-//enmascarar los digitos
-
-function enmascarar(){
-  let numero = document.getElementById("numero-tarjeta");
-  let digitosTC = numero.value;
-  let cantidadDigitos = digitosTC.length;
-
-  if(cantidadDigitos > 4){
-    let digitosAEnmascarar = cantidadDigitos -4 ;
-    for(let i=0 ; i< digitosEnmascarar; i++){
-      digitosTC = digitosTC.replace(digitosTC[i], "*");
-    }
   }
-  document.getElementById("svgnumber").innerHTML = digitosTC;
+
+
+// enmascarar los digitos
+
+function enmascarar() {
+    let numero = document.getElementById("numero-tarjeta");
+    let digitosTC = numero.value;
+    let cantidadDigitos = digitosTC.length;
+
+    if (cantidadDigitos > 4) {
+        let digitosAEnmascarar = cantidadDigitos - 4;
+        for (let i = 0; i < digitosAEnmascarar; i++) {
+            digitosTC = digitosTC.replace(digitosTC[i], "*");
+        }
+    }
+    document.getElementById("svgnumber").innerHTML = digitosTC;
 }
 
-document.getElementById("btn-validar").addEventListener("click", function(){
-   let valida = validartarjeta();
-  if(valida){
-     enmascarar();
-     document.getElementById("numero-tarjeta").value = "";
-     this.hidden = true;
-   }
+document.getElementById("btn-validar").addEventListener("click", function () {
+    let valida = validarTarjeta();
+    if (valida) {
+        enmascarar();
+        document.getElementById("numero-tarjeta").value = "";
+        this.hidden = true;
+    }
 })
